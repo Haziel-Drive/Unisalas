8f309150ed7d64d7c1dcc30e3761f06f81e05bba
Edgar Haziel Reyna Garcia <reynagarciahaziel@gmail.com>
2025-11-19 18:01:16 -0600

    feat: Integracion de Express, Cors

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: 'Servidor funcionando',
+        port: PORT
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(` Servidor corriendo en http://localhost:${PORT}`);
+});

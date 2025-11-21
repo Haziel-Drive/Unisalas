b3efa0f4b6ac40021c60201ff709bc884e394a94
Adrian Hernandez Marin <eladr1shdzm@gmail.com>
2025-11-21 17:25:26 -0600

    feat: Se agregan las rutas a las vistas HTML (login, inicio, etc.)

--- server/index.js
+++ server/index.js
@@ -16,6 +16,34 @@ app.get('/', (req, res) => {
     });
 });
 
+// --- RUTAS DE VISTAS (HTML) ---
+
+// 1. VISTA: LOGIN
+// Cuando entren a 'localhost:3000/' o 'localhost:3000/login'
+app.get('/', (req, res) => {
+    res.redirect('/login');
+});
+
+app.get('/login', (req, res) => {
+    // Busca el archivo en la carpeta 'views'
+    res.sendFile(path.join(__dirname, 'views', 'login.html'));
+});
+
+// 2. VISTA: INICIO
+app.get('/inicio', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'inicio.html'));
+});
+
+// 3. VISTA: OPCIONES
+app.get('/opciones', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'opciones.html'));
+});
+
+// 4. VISTA: FORMULARIO
+app.get('/formulario', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'formulario.html'));
+});
+
 app.listen(PORT, () => {
     console.log(` Servidor corriendo en http://localhost:${PORT}`);
 });
